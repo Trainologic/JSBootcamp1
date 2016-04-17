63c5653589d814592d8e9f47bbd0c4d7ed63a6a1
oricalvo <ori.calvo@gmail.com>
2016-04-17 20:57:13 +0300

    More

--- 08 Angular/16 Angular+Typescript+SASS+SystemJS/app/components/app.ts
+++ 08 Angular/16 Angular+Typescript+SASS+SystemJS/app/components/app.ts
@@ -1,9 +1,28 @@
 import {module} from "../common/module";
 import "./clock";
 import {ClockComponent} from "./clock";
+import "./contactList";
+import {Contact} from "../services/contactService";
 
 export class AppComponent {
+    contacts1: Contact[];
+    contacts2: Contact[];
+
     constructor() {
+        this.contacts1 = [
+            {id:1, name: "Ori"},
+            {id:2, name: "Roni"},
+        ];
+
+        this.contacts2 = [
+            {id:2, name: "Roni"},
+            {id:1, name: "Ori"},
+        ];
+    }
+
+    onReverse() {
+        //alert("R");
+        this.contacts1.reverse();
     }
 
     onTick(sender: ClockComponent, time) {
